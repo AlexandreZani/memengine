b8a2464b8c868351e0f42215c9befee2dfe7d919
Alexandre Zani <alexandre.zani@gmail.com>
2013-01-12 12:12:29 -0800

    Put a global lock on the lru queue.

--- lru.c
+++ lru.c
@@ -5,6 +5,7 @@ create_lru_queue() {
   lru_queue_t *lru_queue = malloc(sizeof(lru_queue_t));
   lru_queue->most_recent = NULL;
   lru_queue->least_recent = NULL;
+  pthread_mutex_init(&(lru_queue->mutex), NULL);
   return lru_queue;
 }
 
@@ -17,11 +18,13 @@ destroy_lru_queue(lru_queue_t *lru_queue) {
 
 void
 lru_add_new_entry(lru_queue_t *lru_queue, cache_entry_t *cache_entry) {
+  pthread_mutex_lock(&(lru_queue->mutex));
   if (lru_queue->most_recent == NULL) {
     lru_queue->most_recent = cache_entry;
     lru_queue->least_recent = cache_entry;
     cache_entry->lru_more_recent = NULL;
     cache_entry->lru_less_recent = NULL;
+    pthread_mutex_unlock(&(lru_queue->mutex));
     return;
   }
 
@@ -29,11 +32,14 @@ lru_add_new_entry(lru_queue_t *lru_queue, cache_entry_t *cache_entry) {
   cache_entry->lru_less_recent = lru_queue->most_recent;
   cache_entry->lru_more_recent = NULL;
   lru_queue->most_recent = cache_entry;
+  pthread_mutex_unlock(&(lru_queue->mutex));
 }
 
 bool
 lru_bump_used_entry(lru_queue_t *lru_queue, cache_entry_t *cache_entry) {
+  pthread_mutex_lock(&(lru_queue->mutex));
   if (cache_entry == lru_queue->most_recent) {
+    pthread_mutex_unlock(&(lru_queue->mutex));
     return true;
   }
   cache_entry->lru_more_recent->lru_less_recent = cache_entry->lru_less_recent;
@@ -51,13 +57,16 @@ lru_bump_used_entry(lru_queue_t *lru_queue, cache_entry_t *cache_entry) {
   cache_entry->lru_more_recent = NULL;
   lru_queue->most_recent->lru_more_recent = cache_entry;
   lru_queue->most_recent = cache_entry;
+  pthread_mutex_unlock(&(lru_queue->mutex));
   return true;
 }
 
 cache_entry_t*
 lru_pop_least_recent(lru_queue_t *lru_queue) {
+  pthread_mutex_lock(&(lru_queue->mutex));
   cache_entry_t *ret = lru_queue->least_recent;
   if (ret == NULL) {
+    pthread_mutex_unlock(&(lru_queue->mutex));
     return ret;
   }
 
@@ -70,16 +79,19 @@ lru_pop_least_recent(lru_queue_t *lru_queue) {
   }
   ret->lru_less_recent = NULL;
   ret->lru_more_recent = NULL;
+  pthread_mutex_unlock(&(lru_queue->mutex));
   return ret;
 }
 
 bool
 lru_remove_entry(lru_queue_t *lru_queue, cache_entry_t * cache_entry) {
+  pthread_mutex_lock(&(lru_queue->mutex));
   // If the entry does not point to a less recent entry and it is not itself
   // the least recent entry this can only mean that it is not part of the lru
   // queue.
   if (cache_entry->lru_less_recent == NULL &&
       lru_queue->least_recent != cache_entry) {
+    pthread_mutex_unlock(&(lru_queue->mutex));
     return false;
   }
 
@@ -95,5 +107,6 @@ lru_remove_entry(lru_queue_t *lru_queue, cache_entry_t * cache_entry) {
   }
   cache_entry->lru_less_recent = NULL;
   cache_entry->lru_more_recent = NULL;
+  pthread_mutex_unlock(&(lru_queue->mutex));
   return true;
 }
--- lru.h
+++ lru.h
@@ -2,11 +2,13 @@
 #define __LRU_H
 
 #include "cache_entry.h"
+#include <pthread.h>
 #include <stdbool.h>
 
 typedef struct {
   cache_entry_t *most_recent;
   cache_entry_t *least_recent;
+  pthread_mutex_t mutex;
 } lru_queue_t;
 
 extern lru_queue_t *create_lru_queue();
